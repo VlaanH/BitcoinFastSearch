1a13a8a56369454596bc3ab23d9ccfdd5bc918c3
vlanh <vlan.h@yandex.ua>
2021-07-23 16:57:40 +0300

    Added method to save the result

--- AddressData/Address.cs
+++ AddressData/Address.cs
@@ -1,5 +1,8 @@
+using System;
+using BitcoinFastSearch.Balance;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.Json;
 
 namespace BitcoinFastSearch.AddressData
@@ -15,7 +18,23 @@ namespace BitcoinFastSearch.AddressData
 
         }
 
-        public static List<Data> Read(string patch)
+
+
+       public static void Write(string path,List<Data> addressData)
+       {
+           using (FileStream streamWriter = new FileStream(path + "/res.txt", FileMode.Append, FileAccess.Write))
+           {
+               for (int i = 0; i < addressData.Count; i++)
+               {
+                   streamWriter.Write(Encoding.Default.GetBytes($"Address:{addressData[i].Address} PrivateKey:{addressData[i].PrivateKey} \n"));
+               }
+          
+           }
+           
+       }
+
+
+       public static List<Data> Read(string patch)
         {
             List<Data> allAddressData = new List<Data>();
             StreamReader streamReader = new StreamReader(patch);
